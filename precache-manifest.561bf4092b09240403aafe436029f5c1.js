ae8f08b8c4a7f6645ee1e384c964d6bc3379ff6c
lw <1109160884@qq.com>
2022-06-29 22:14:18 +0800

    deploy

---
+++ precache-manifest.561bf4092b09240403aafe436029f5c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08e5cb7332ab714922b146bd9d6e27cb",
+    "url": "/535-money-website/QRcode.png"
+  },
+  {
+    "revision": "a4ed7bab0fe288efc54f",
+    "url": "/535-money-website/css/app.052dcb33.css"
+  },
+  {
+    "revision": "91885cda185fb5650358facf105a22c7",
+    "url": "/535-money-website/index.html"
+  },
+  {
+    "revision": "a4ed7bab0fe288efc54f",
+    "url": "/535-money-website/js/app.6e1c8ee0.js"
+  },
+  {
+    "revision": "64d4a55797c6ca567618",
+    "url": "/535-money-website/js/chunk-vendors.f3824581.js"
+  },
+  {
+    "revision": "222728f43e3bdd7772f54b1465a9719a",
+    "url": "/535-money-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/535-money-website/robots.txt"
+  }
+]);
